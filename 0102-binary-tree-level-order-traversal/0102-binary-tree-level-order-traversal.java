a126590aef9f1dd84ae7c6d285586ed5aea96f13
Ashish Sharma <144713656+athexor@users.noreply.github.com>
2026-07-27 18:16:32 +0530

    Time: 1 ms (95.87%), Space: 46.7 MB (62.41%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -4,9 +4,9 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *     TreeNode(){}
+ *     TreeNode(int val){ this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right){
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
@@ -16,26 +16,26 @@
 
 
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode A) {
+    public List<List<Integer>> levelOrder(TreeNode A){
         List<List<Integer>> result = new ArrayList<>();
 
-        if (A == null) {
+        if(A == null){
             return result;
         }
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(A);
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int size = queue.size();
             List<Integer> level = new ArrayList<>();
 
-            for (int i = 0; i < size; i++) {
+            for(int i = 0; i < size; i++){
                 TreeNode temp = queue.poll();
                 level.add(temp.val);
 
-                if (temp.left != null) queue.add(temp.left);
-                if (temp.right != null) queue.add(temp.right);
+                if(temp.left != null)queue.add(temp.left);
+                if(temp.right != null)queue.add(temp.right);
             }
 
             result.add(level);
@@ -47,11 +47,11 @@ class Solution {
 
 /** 
  * class Solution {
- *     public List<List<Integer>> levelOrder(TreeNode A) {
+ *     public List<List<Integer>> levelOrder(TreeNode A){
  *         Queue<TreeNode> queue = new LinkedList<>();
  *         List<List<Integer>> list = new ArrayList<>();
  * 
- *         if (A == null) return list;
+ *         if(A == null)return list;
  * 
  *         queue.add(A);
  *         queue.add(null);
@@ -59,18 +59,18 @@ class Solution {
  *         list.add(new ArrayList<>()); // First level
  *         int index = 0;
  *
- *         while (!queue.isEmpty()) {
+ *         while(!queue.isEmpty()){
  *             TreeNode temp = queue.remove();
- *             if (temp == null) {
- *                 if (!queue.isEmpty()) { // Only add another marker if nodes remain
+ *             if(temp == null){
+ *                 if(!queue.isEmpty()){ // Only add another marker if nodes remain
  *                     queue.add(null);
  *                     index++;
  *                     list.add(new ArrayList<>()); // Create list for next level
  *                 }
  *             } else {
  *                 list.get(index).add(temp.val);
- *                 if (temp.left != null) queue.add(temp.left);
- *                 if (temp.right != null) queue.add(temp.right);
+ *                 if(temp.left != null)queue.add(temp.left);
+ *                 if(temp.right != null)queue.add(temp.right);
  *             }
  *         }
  
