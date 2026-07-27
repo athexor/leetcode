a3dadf271d7c4e3d2930c01763d79025e2ee0d34
Ashish Sharma <144713656+athexor@users.noreply.github.com>
2026-07-27 18:15:39 +0530

    Time: 1 ms (95.87%), Space: 46.8 MB (42.06%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -46,38 +46,35 @@ class Solution {
 }
 
 /** 
-*class Solution {
-*    public List<List<Integer>> levelOrder(TreeNode A) {
-*        Queue<TreeNode> queue = new LinkedList<>();
-*        List<List<Integer>> list = new ArrayList<>();
-*
-*        if (A == null) return list;
-*
-*        queue.add(A);
-*        queue.add(null);
-*
-*        list.add(new ArrayList<>()); // First level
-*
-*        int index = 0;
-*
-*        while (!queue.isEmpty()) {
-*            TreeNode temp = queue.remove();
-*
-*            if (temp == null) {
-*                if (!queue.isEmpty()) { // Only add another marker if nodes remain
-*                    queue.add(null);
-*                    index++;
-*                    list.add(new ArrayList<>()); // Create list for next level
-*                }
-*            } else {
-*                list.get(index).add(temp.val);
-*
-*                if (temp.left != null) queue.add(temp.left);
-*                if (temp.right != null) queue.add(temp.right);
-*            }
-*        }
-*
-*        return list;
-*    }
-*}
-*/
+ * class Solution {
+ *     public List<List<Integer>> levelOrder(TreeNode A) {
+ *         Queue<TreeNode> queue = new LinkedList<>();
+ *         List<List<Integer>> list = new ArrayList<>();
+ * 
+ *         if (A == null) return list;
+ * 
+ *         queue.add(A);
+ *         queue.add(null);
+ * 
+ *         list.add(new ArrayList<>()); // First level
+ *         int index = 0;
+ *
+ *         while (!queue.isEmpty()) {
+ *             TreeNode temp = queue.remove();
+ *             if (temp == null) {
+ *                 if (!queue.isEmpty()) { // Only add another marker if nodes remain
+ *                     queue.add(null);
+ *                     index++;
+ *                     list.add(new ArrayList<>()); // Create list for next level
+ *                 }
+ *             } else {
+ *                 list.get(index).add(temp.val);
+ *                 if (temp.left != null) queue.add(temp.left);
+ *                 if (temp.right != null) queue.add(temp.right);
+ *             }
+ *         }
+ 
+ *         return list;
+ *     }
+ * }
+ */
