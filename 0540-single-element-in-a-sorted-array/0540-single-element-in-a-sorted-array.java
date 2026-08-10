e647921bd02baaddb3dba2a47f40febe20269359
Ashish Sharma <144713656+athexor@users.noreply.github.com>
2026-08-10 20:29:04 +0530

    Time: 0 ms (100%), Space: 52.7 MB (78.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,23 +6,41 @@ class Solution {
 
         while(l<=r){
             int mid = l+(r-l)/2;
-            
             if(
                 (mid == 0   || A[mid] != A[mid-1]) &&
-                (mid == n-1 || A[mid] != A[mid+1])
+                (mid == n-1 || A[mid] != A[mid+1]) 
             ){
                 return A[mid];
             }
-            
-            if(mid !=0 && A[mid] == A[mid-1])  mid--;
 
-            if(mid % 2 == 0){
-                l = mid+2;
+            int first = mid;
+            if(mid != 0 && A[mid] == A[mid-1])
+                first--;
+            
+            if(first % 2 == 0){
+                l = mid+1;
             }else{
                 r = mid-1;
-            }
+            }   
         }
 
         return -1;
     }
 }
+
+
+/** 
+ * if(n == 1) return A[0];
+ * We don't need separate checks for the first and last elements. 
+ * 
+ * The conditions (mid == 0) and (mid == n - 1) already handle
+ * this specific cases inside the main condition. 
+ * 
+ * We can add these checks as well.
+ * if(A[0] != A[1]) return A[0];
+ * if(A[n-1] != A[n-2]) return A[n-1];
+ * This will save some extra time in specific edge cases.
+ * 
+ * Doubt: here using `mid` instead of `first` for updating l/r (l=mid+1, r=mid-1)
+ * does this cause any correctness/infinite-loop issue?
+**/
