e1d340be81564a2037e1f42f5f0f1cc1e050f8b1
Ashish Sharma <144713656+athexor@users.noreply.github.com>
2026-08-15 17:16:45 +0530

    Time: 0 ms (100%), Space: 43 MB (35.25%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -16,8 +16,20 @@ class Solution {
         while(fP != null && fP.next != null){
             sP = sP.next;
             fP = fP.next.next;
-        }   
+        }
 
         return sP;
     }
 }
+
+/**
+ * Done. No need to handle the head == null condition,
+ * as the while loop already handles this case.
+ *
+ * In the brute force solution, we have to traverse n times first to count
+ * the number of elements, then jump n/2 times starting from index 0:
+ *
+ * for (i = 0; i <= n/2 - 1; i++)
+ *
+ * i.e., jump = 0; jump <= n/2 - 1; jump++
+ */
